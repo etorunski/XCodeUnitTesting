cf063266265dc188dfa26cb4cf92f1d1e04184a7
Eric Torunski <etorunski@gmail.com>
2019-01-30 11:48:45 -0500

    Finished example

--- XCodeUnitTesting/ViewController.swift
+++ XCodeUnitTesting/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var switchEnableButton: UISwitch!
+    
+    @IBOutlet weak var textFieldResult: UITextField!
+    @IBAction func testButtonTapped(_ sender: Any) {
+        if(switchEnableButton.isOn)
+        {
+            textFieldResult.text = "The button works!"
+        }
+        else{
+            textFieldResult.text="The switch is turned off"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
