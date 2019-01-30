338a498066497fd4cd912b4a355ff252f88af22d
Eric Torunski <etorunski@gmail.com>
2019-01-30 10:56:01 -0500

    Initial Commit

---
+++ XCodeUnitTesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XCodeUnitTesting
+//
+//  Created by Eric Torunski on 2019-01-30.
+//  Copyright © 2019 Eric Torunski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
