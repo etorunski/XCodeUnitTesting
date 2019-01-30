fbdca6e2117c8bc42570371de22a88131dff81bc
Eric Torunski <etorunski@gmail.com>
2019-01-30 12:09:36 -0500

    added iOS test case

--- XCodeUnitTestingUITests/TestMyApplication.swift
+++ XCodeUnitTestingUITests/TestMyApplication.swift
@@ -1,5 +1,5 @@
 //
-//  XCodeUnitTestingUITests.swift
+//  TestMyApplication.swift
 //  XCodeUnitTestingUITests
 //
 //  Created by Eric Torunski on 2019-01-30.
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class XCodeUnitTestingUITests: XCTestCase {
+class TestMyApplication: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -20,8 +20,6 @@ class XCodeUnitTestingUITests: XCTestCase {
         XCUIApplication().launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-        
-        
     }
 
     override func tearDown() {
@@ -31,7 +29,11 @@ class XCodeUnitTestingUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-                
+        
+        let app = XCUIApplication()
+        app.buttons["Tap button to do something"].tap()
+        let typedText = app.textFields["Result of test"].value as! String
+        XCTAssertEqual("this works", typedText, "text field is not correct")
     }
 
 }
